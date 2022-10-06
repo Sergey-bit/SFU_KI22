f5f4bc7db0aa72821e7eb4a5f802907dbe5ffe3d
Sergey-bit <71939250+Sergey-bit@users.noreply.github.com>
2022-10-07 00:53:25 +0700

    Add files via upload

---
+++ ОП/3Practic/TASK4/3preview.cpp
@@ -0,0 +1,33 @@
+//Елин Максим Алексеевич
+//КИ22-07Б
+//практика номер 3
+//Вариант 4
+
+#include <stdio.h>
+#include <stdlib.h>
+
+int main() {
+	system("chcp 1251");
+	system("cls");
+
+	double x1, y1, exam;
+	printf("Введите x: ");
+	scanf_s("%lf", &x1);
+	printf("Введите y: ");
+	scanf_s("%lf", &y1);
+	exam = 4 * x1 - 3 - y1;
+	if (exam == 0) {
+		printf("YES ");
+		return 0;
+	}
+
+	if (exam < 0) {
+		printf("UP ");
+		return 0;
+	}
+
+	printf("DOWN ");
+
+	system("pause");
+	return 0;
+}
