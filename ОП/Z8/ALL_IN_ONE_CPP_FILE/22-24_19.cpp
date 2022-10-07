6f0153bb53090380344d8bd8eb12ba5be3110164
Sergey-bit <71939250+Sergey-bit@users.noreply.github.com>
2022-10-07 17:09:15 +0700

    Update 22-24_19.cpp
    Signed-off-by: Sergey-bit <71939250+Sergey-bit@users.noreply.github.com>

--- ОП/Z8/ALL_IN_ONE_CPP_FILE/22-24_19.cpp
+++ ОП/Z8/ALL_IN_ONE_CPP_FILE/22-24_19.cpp
@@ -58,40 +58,6 @@ int splice_array(int** const arr1, int** const arr2, const size_t len, const siz
 	return len * sublen * 2;
 }
 
-// Additional
-int* get_array(const size_t sublen)
-{
-	int* arr = new int[sublen];
-
-	for (size_t i = 0; i < sublen; i++)
-	{
-		arr[i] = i;
-	}
-
-	return arr;
-}
-int** init_2array(const size_t len, const size_t sublen)
-{
-	int** arr = new int*[len];
-
-	for (size_t i = 0; i < len; i++)
-	{
-		arr[i] = get_array(sublen);
-	}
-
-	return arr;
-}
-
-void free_2array(int** array, const size_t length)
-{
-	for (size_t i = 0; i < length; i++)
-	{
-		delete[] *(array + i);
-	}
-
-	delete[] array;
-}
-
 int main()
 {
 	int** array = init_2array(LENGTH, LENGTH);
