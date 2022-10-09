11f305dd943c1ee7b5003d88d3f347e793569688
Sergey-bit <71939250+Sergey-bit@users.noreply.github.com>
2022-10-09 13:28:19 +0700

    Add files via upload
    Signed-off-by: Sergey-bit <71939250+Sergey-bit@users.noreply.github.com>

---
+++ ОП/2Practic/TASK5/задание 5.cpp
@@ -0,0 +1,19 @@
+﻿#include <iostream>
+#include <clocale>
+using namespace std;
+int main()
+{
+	setlocale(0, "");
+	cout << "Введите D (КУРС ДОЛЛАРА)\n";
+	int D;
+	cin >> D;
+	cout << "Введите E (КУРС ЕВРО) \n";
+	int E;
+	cin >> E;
+	cout << "Введите K (КОЛ-ВО РУБЛЕЙ В КАРМАНЕ) \n";
+	int K;
+	cin >> K;
+
+	cout << "МОЖНО РАЗМЕНЯТЬ ДОЛЛАРОВ = " << K / D << "\n";
+	cout << "МОЖНО РАЗМЕНЯТЬ ЕВРО = " << K / E << "\n";
+}
