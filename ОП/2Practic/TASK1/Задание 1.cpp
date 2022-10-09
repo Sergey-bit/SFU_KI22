ca403e488e5c33c127b3ca046f6b6eed36505c31
Sergey-bit <71939250+Sergey-bit@users.noreply.github.com>
2022-10-09 13:25:23 +0700

    Add files via upload
    Signed-off-by: Sergey-bit <71939250+Sergey-bit@users.noreply.github.com>

---
+++ ОП/2Practic/TASK1/Задание 1.cpp
@@ -0,0 +1,18 @@
+﻿#include <iostream>
+#include <clocale>
+using namespace std;
+int main()
+{
+	setlocale(0, "");
+	cout << "Введите N (КОЛ-ВО МЕСЯЦЕВ)\n";
+	int N;
+	cin >> N;
+	cout << "Введите K (ГОДОВЫЕ)\n";
+	int K;
+	cin >> K;
+	cout << "Введите S (ВНЕСЕНО) \n";
+	int S;
+	cin >> S;
+
+	cout << "ПРИЧИТАЕТСЯ КЛИЕНТУ = " << (S + (S * K)) * N;
+}
