831e44c4818769e17fd9e68ab9944b7041d50ea2
Sergey-bit <71939250+Sergey-bit@users.noreply.github.com>
2022-10-09 13:30:33 +0700

    Add files via upload
    Signed-off-by: Sergey-bit <71939250+Sergey-bit@users.noreply.github.com>

---
+++ ОП/5Practic/6VAR/F_Prakt_rab_5_zadanie_6__finalny_variant.cpp
@@ -0,0 +1,29 @@
+﻿#include <iostream>
+#include <clocale>
+#include <string>
+using namespace std;
+
+void f1(int N) {
+	for (N; N != 0; N = N - 1) {
+		string str = to_string(N);
+		int Z = str.length();
+		int K = N;
+		int s = 0;
+		while (Z != 0) {
+			s = s + (K % 10);
+			K = K / 10;
+			Z = Z - 1;
+		}
+		if (N % s == 0) {
+			cout << N << endl;
+		}
+	}
+}
+
+int main() {
+	setlocale(0, "");
+	int N;
+	cout << "Введите значение N ";
+	cin >> N;
+	f1(N);
+}
