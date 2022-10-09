af6225601ece52c9145058e5714a4c381b0b3eb4
Sergey-bit <71939250+Sergey-bit@users.noreply.github.com>
2022-10-09 13:26:06 +0700

    Add files via upload
    Signed-off-by: Sergey-bit <71939250+Sergey-bit@users.noreply.github.com>

---
+++ ОП/2Practic/TASK2/Задание 2.cpp
@@ -0,0 +1,20 @@
+﻿#include <iostream>
+#include <clocale>
+using namespace std;
+int main()
+{
+	setlocale(0, "");
+	cout << "Введите N (КОЛ-ВО абитуриентов на экзамене)\n";
+	int N;
+	cin >> N;
+	cout << "Введите M (Сколько сдали математику)\n";
+	int M;
+	cin >> M;
+	cout << "Введите F (Сколько сдали физику) \n";
+	int F;
+	cin >> F;
+	cout << "Введите L (не сдали ни одного предмета) \n";
+	int L;
+	cin >> L;
+	cout << "Кол-во студентов, которые сдали оба предмета " << min((N - L) - M, (N - L) - F);
+}
