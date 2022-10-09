e5580fd9e3309570b14fcda5041cd189ef66dcfb
Sergey-bit <71939250+Sergey-bit@users.noreply.github.com>
2022-10-09 13:27:13 +0700

    Add files via upload
    Signed-off-by: Sergey-bit <71939250+Sergey-bit@users.noreply.github.com>

---
+++ ОП/2Practic/TASK3/задание 3.cpp
@@ -0,0 +1,22 @@
+﻿#include <iostream>
+#include <clocale>
+using namespace std;
+int main()
+{
+	setlocale(0, "");
+	cout << "Введите v1 (объем воды 1)\n";
+	int v1;
+	cin >> v1;
+	cout << "Введите v2 (объем воды 2)\n";
+	int v2;
+	cin >> v2;
+	cout << "Введите t1 (температура воды 1) \n";
+	int t1;
+	cin >> t1;
+	cout << "Введите t2 (температура воды 2) \n";
+	int t2;
+	cin >> t2;
+
+	cout << "объем образовавшейся смеси = " << v1 + v2 << "\n";
+	cout << "температура образовавшейся смеси = " << (t1 + t2) / 2 << "\n";
+}
